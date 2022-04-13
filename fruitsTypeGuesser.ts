001e8ac35127f7bc8a486e63d2d522dbeae102ea
Alex Cebotari <alexandr.cebotari@gmail.com>
2022-04-13 13:32:05 +0200

    fix: added description

--- fruitsTypeGuesser.ts
+++ fruitsTypeGuesser.ts
@@ -5,43 +5,6 @@ type FruitItem = {
     type?: string;
 }
 
-const items: Array<FruitItem> = [
-    {
-        id: 12412321,
-        countryOfOrigin: 'MD',
-        weight: 50,
-        type: 'apple'
-    },
-    {
-        id: 21513133,
-        countryOfOrigin: 'MD',
-        weight: 45,
-    },
-    {
-        id: 23521252,
-        countryOfOrigin: 'MD',
-        weight: 70,
-        type: 'apricot',
-    },
-    {
-        id: 62324435,
-        countryOfOrigin: 'UK',
-        weight: 50,
-        type: 'watermelon'
-    },
-    {
-        id: 94817355,
-        countryOfOrigin: 'UK',
-        weight: 190,
-    },
-    {
-        id: 36503123,
-        countryOfOrigin: 'UK',
-        weight: 220,
-        type: 'grape',
-    },
-];
-
 /* 
     Fruits-sorting machine requires each fruit to have type
     Some of fruits were not scanned properly and are missing type parameter
@@ -50,6 +13,87 @@ const items: Array<FruitItem> = [
     - it's coming from the same country
     - the difference in weight is not greater than 50 gramms
     - if it fitts more than 1 type of the fruit, use the with the smalles weight difference
+
+    Input example:
+
+    const items: Array<FruitItem> = [
+        {
+            id: 12412321,
+            countryOfOrigin: 'MD',
+            weight: 90,
+            type: 'apple'
+        },
+        {
+            id: 21513133,
+            countryOfOrigin: 'MD',
+            weight: 45,
+        },
+        {
+            id: 23521252,
+            countryOfOrigin: 'MD',
+            weight: 70,
+            type: 'apricot',
+        },
+        {
+            id: 62324435,
+            countryOfOrigin: 'UK',
+            weight: 50,
+            type: 'pear'
+        },
+        {
+            id: 94817355,
+            countryOfOrigin: 'UK',
+            weight: 190,
+        },
+        {
+            id: 36503123,
+            countryOfOrigin: 'UK',
+            weight: 220,
+            type: 'grape',
+        },
+    ];
+
+    The function should return this enhance version:
+    
+    const items: Array<FruitItem> = [
+        {
+            id: 12412321,
+            countryOfOrigin: 'MD',
+            weight: 90,
+            type: 'apple'
+        },
+        {
+            id: 21513133,
+            countryOfOrigin: 'MD',
+            weight: 45,
+            type: 'apricot',
+        },
+        {
+            id: 23521252,
+            countryOfOrigin: 'MD',
+            weight: 70,
+            type: 'apricot',
+        },
+        {
+            id: 62324435,
+            countryOfOrigin: 'UK',
+            weight: 50,
+            type: 'pear'
+        },
+        {
+            id: 94817355,
+            countryOfOrigin: 'UK',
+            weight: 190,
+            type: 'grape',
+        },
+        {
+            id: 36503123,
+            countryOfOrigin: 'UK',
+            weight: 220,
+            type: 'grape',
+        },
+    ];
+
 */
 
 export const enhanceFruitData = (
